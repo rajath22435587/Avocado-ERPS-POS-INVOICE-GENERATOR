9a0d890689ecc750a4d19809670bb0c6a6b4f0f1
Rajath Ramakrishna <rajath22435587@gmail.com>
2018-10-16 01:52:34 +0530

    Main Design Generated

---
+++ Avocado-ERPS-POS-INVOICE GENERATOR/Avocado-ERPS-POS-INVOICE GENERATOR-INVOICE GENERATOR SCREEN.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace Avocado_ERPS_POS_INVOICE_GENERATOR
+{
+    public partial class Form2 : MaterialForm
+    {
+        public Form2()
+        {
+            InitializeComponent();
+
+            MaterialSkinManager materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Blue400, Primary.Blue500,
+                Primary.Blue500, Accent.LightBlue200,
+                TextShade.WHITE
+                );
+        }
+    }
+}
